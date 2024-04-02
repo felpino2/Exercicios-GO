fcf187958cf1744fa098d7a982cb30a4a5996e7c
Felipe Augusto Ferreira de Almeida <felipeafa202@gmail.com>
2024-04-02 15:04:42 +0000

    Update aula02ex01.go

--- aula02ex01.go
+++ aula02ex01.go
@@ -1,1 +1,29 @@
 package main
+
+import "fmt"
+
+func main() {
+	var x float32
+	var y float32
+
+	fmt.Print("Qual a coordenada do x?")
+	fmt.Scan(&x)
+	fmt.Println("Qual a coordenada do y?")
+	fmt.Scan(&y)
+
+	if x == 0 && y == 0 {
+		fmt.Println("Origem")
+	} else if x == 0 {
+		fmt.Println("Eixo X")
+	} else if y == 0 {
+		fmt.Println("Eixo Y")
+	} else if x > 0 && y > 0 {
+		fmt.Println("Quadrante 1")
+	} else if x > 0 && y < 0 {
+		fmt.Println("Quadrante 4")
+	} else if x < 0 && y > 0 {
+		fmt.Println("Quadrante 2")
+	} else if x < 0 && y < 0 {
+		fmt.Println("Quadrante 3")
+	}
+}
